6e11f3f6da074ed2c539f8a61fa32f345c8c6814
AkashNerkar007 <akashnerkar121@gmail.com>
2025-07-21 15:44:00 +0530

    Third Phase

--- app/routes/doc.py
+++ app/routes/doc.py
@@ -1,7 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/editor")
-def editor_page():
-    return {"message": "Editor coming soon..."}
+active_connections = []
+
+@router.get("/editor", response_class=HTMLResponse)
+async def get_editor(request: Request):
+    return templates.TemplateResponse("editor.html", {"request": request})
+
+@router.websocket("/ws/editor")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    active_connections.append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Broadcast to all except sender
+            for conn in active_connections:
+                if conn != websocket:
+                    await conn.send_text(data)
+    except WebSocketDisconnect:
+        active_connections.remove(websocket)
--- app/static/main.js
+++ app/static/main.js
@@ -1,1 +1,16 @@
-console.log("Static folder working!");
+const editor = document.getElementById("editor");
+const socket = new WebSocket(`ws://${location.host}/ws/editor`);
+
+let skipBroadcast = false;
+
+socket.onmessage = (event) => {
+  skipBroadcast = true;
+  editor.value = event.data;
+};
+
+editor.addEventListener("input", () => {
+  if (!skipBroadcast) {
+    socket.send(editor.value);
+  }
+  skipBroadcast = false;
+});
